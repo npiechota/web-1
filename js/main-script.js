cf6281fed5bab5d0dc97537b997aa4adf10d35fc
npiechota <hvshd47@gmail.com>
2016-02-20 16:02:01 +0100

    update

---
+++ js/main-script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+	$('.menu-bar').click(function() {
+
+		$('#nav-top').toggleClass('visible');
+
+		$('#nav-top a, .menu-close').click(function() {
+			$('#nav-top').removeClass('visible');
+		});
+
+	});
+
+	$('.darkbox').click(function(e){
+
+		e.preventDefault();
+
+		$('.box-overlay, .box').animate({'opacity': '0.8'}, 300, 'linear');
+		$('.box').animate({'opacity': '1'}, 300, 'linear');
+		$('.box-overlay, .box').css('display', 'block');
+
+		var img_href = $(this).attr('href');
+		$('#box-img').attr('src', img_href);
+
+		$('.close-btn, .box-overlay').click(function(){
+			darkbox_close();
+		});
+
+	});
+
+	$('#nav-about, #nav-more, #nav-gallery, #nav-contact').click(function(e){
+
+		e.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: $( $(this).attr('href') ).offset().top
+		}, 800);
+	});
+
+
+	function darkbox_close() {
+		$('.box-overlay, .box').animate({'opacity': '0'}, 300, 'linear', function(){
+			$('.box-overlay, .box').css('display', 'none');
+			
+		});
+	}
+
+});
